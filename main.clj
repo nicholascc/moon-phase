2f5973cb386b47a78fed84416d2049a2964f6424
Nicholas Charette <charetteni@gmail.com>
2021-03-18 20:25:24 -0500

    made code clearer by using condp instead of cond

--- main.clj
+++ main.clj
@@ -3,29 +3,32 @@
 ; current moon phase is computed relative to the new moon of 2000-01-06
 (def new-moon-start (java.time.LocalDate/of 2000 1 6))
 
+(defn end-of-phase [phase]
+  (- (* phase phase-length) (/ phase-length 2)))
+
 (defn moon-phase [now]
   (let [days-since (.until new-moon-start now java.time.temporal.ChronoUnit/DAYS)
         since-last-new (mod days-since moon-period)
         offsetted-day (+ since-last-new (/ phase-length 2))]
-    (cond
+    (condp > since-last-new
       ; since Java strings are stored with UTF-16,
       ; we have to represent emoji as integers, and
       ; later convert them into strings.
-      (< offsetted-day (* 1 phase-length)) 127761
-      (< offsetted-day (* 2 phase-length)) 127762
-      (< offsetted-day (* 3 phase-length)) 127763
-      (< offsetted-day (* 4 phase-length)) 127764
-      (< offsetted-day (* 5 phase-length)) 127765
-      (< offsetted-day (* 6 phase-length)) 127766
-      (< offsetted-day (* 7 phase-length)) 127767
-      (< offsetted-day (* 8 phase-length)) 127768
-      true                                 127761)))
+      (end-of-phase 1) 127761
+      (end-of-phase 2) 127762
+      (end-of-phase 3) 127763
+      (end-of-phase 4) 127764
+      (end-of-phase 5) 127765
+      (end-of-phase 6) 127766
+      (end-of-phase 7) 127767
+      (end-of-phase 8) 127768
+      127761)))
 
 (defn emoji-int->string [emoji]
   (String. (int-array [emoji]) 0 1))
 
 (-> (java.time.LocalDate/now)
-    ; (java.time.LocalDate/of YEAR MONTH DAY) ; uncomment this and comment out the above line to use your own date - i use this for testing
+    ;(java.time.LocalDate/of YEAR MONTH DAY) ; uncomment this and comment out the above line to use your own date - i use this for testing
     moon-phase
     emoji-int->string
     println)
