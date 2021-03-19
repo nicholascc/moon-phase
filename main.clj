a84220ffadb3bddf729fca55a1c9d9c49351f71a
Nicholas <34197135+nicholascc@users.noreply.github.com>
2021-03-18 22:33:27 -0500

    removed commented-out ‘use your own date’ option

--- main.clj
+++ main.clj
@@ -27,7 +27,6 @@
   (String. (int-array [emoji]) 0 1))
 
 (-> (java.time.LocalDate/now)
-    ;(java.time.LocalDate/of YEAR MONTH DAY) ; uncomment this and comment out the above line to use your own date - i use this for testing
     moon-phase
     emoji-int->string
     println)
